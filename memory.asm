16e212414fce7a09419ac3303fdda94a7716c24b
developer328 <Nikoloz1F5@gmail.com>
2023-10-31 23:13:17 +0400

    ifElse

--- memory.asm
+++ memory.asm
@@ -1,39 +1,45 @@
 ;28-oct-2023 17:00PM - start
 ;print rsp value
 
-
-; ......
-; :    :
-; :    :
-; :....:
-
 	global _start
 	
 	_start:	
 
 section .text
-	
-	;this code purpose is only print "9+A \n", yet;
-	;print 5 for test;
+
+	; now it is if_else 
 	;problem - how to print numbers?!
 	
-	push rbp
-	mov rbp, rsp
-	sub rsp, 0x14
-	mov DWORD [rbp-4], 5
+	push rbp	; hold old rsp location
+	mov rbp, rsp	; save new rsp addres on ..^
+	sub rsp, 0x14	; 14 word for stack memory	
+	mov DWORD [rbp-8], 4	; store dec 5 on rbp-4 addr
+	mov DWORD [rbp-14], 5
+	
+	mov eax, DWORD [rbp-8]
+	cmp eax, DWORD [rbp-14]
+	jle .L2	
 	mov rax, 1	
 	mov rdi, 1
-	mov rsi, abc 
-	mov rdx, 30 
+	mov rsi, str0
+	mov rdx, 2
+	syscall
+	int 80h
+	jmp .L3
+	.L2:
+	mov rax, 1	
+	mov rdi, 1
+	mov rsi, str1
+	mov rdx, 2
 	syscall
 	int 80h	
 
+	.L3:
 	;exit
 	mov rax, 60
 	syscall
 	int 80h
 
 section .data
-
-	abc db "abcdefghijklmnopqrstuvwxyz", 0xa
-	;num db 0x2E, 0x2E, 0x2E, 0x2E, 0x2E, 0x2E, 0xa, 0x3A, 0x20, 0x20, 0x20, 0x20, 0x3A, 0xa, 0x3A, 0x20, 0x20, 0x20, 0x20, 0x3A, 0xa, 0x3A, 0x2E, 0x2E, 0x2E, 0x2E, 0x3A, 0xa
+	str0 db "1",0xa
+	str1 db "2",0xa
